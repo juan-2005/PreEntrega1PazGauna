13374d5e8783fcbafd7ddd97f491c5ad73c936c0
unknown <pazgaunajuanpablo@gmail.com>
2023-07-18 23:10:27 -0300

    first commit

--- Main.js
+++ Main.js
@@ -1,17 +1,17 @@
 
 
-alert("Bienvenidos a Banco Conversion AR")
-let entrada = prompt ("Ingresar nombre")
+alert("Bienvenidos a Banco Conversion AR");
+let entrada = prompt ("Ingresar nombre");
 let salida = entrada + "" ;
 alert(salida)
 
 
 let number;
 if (number=""){
-alert("No puede realizar conversion")
+alert("No puede realizar conversion");
 }
 else{
-alert(" Puede realizar conversion")
+alert(" Puede realizar conversion");
 }
 
 
@@ -19,15 +19,15 @@ let cantidad = Number (prompt("Ingrese cantidad de dinero a convertir"));
 
 for (let i = 1; i <= 10; i++){
     let resultado = cantidad*i;
-    alert(cantidad + "Peso/s" + "X" + i + "Dolar" +" = " + resultado + "Dolares");
+    alert(cantidad + "Peso/s" + "X" + i + "Dolar/es" +" = " + resultado + "Dolar/es");
 }
 
 
 function cierre (){
     let entrada;
-    alert( salida + "ya no requiere de nuestros servicios")
+    alert( salida + "ya no requiere de nuestros servicios");
 }
-cierre()
+cierre();
 
 
 
